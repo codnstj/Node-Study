b41eba4af556186bdd2e2ca2a26ce7a643ff5ece
lot8229 <lot8229@kakao.com>
2021-09-27 09:58:09 +0900

    Delete All Projects

--- 회원가입&로그인 구현(mongodb)/config/dev.js
+++
@@ -1,3 +0,0 @@
-module.exports ={
-  mongoURI : 'mongodb+srv://Seochaewoon:1234@cluster.ynchd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-}
--- 회원가입&로그인 구현(mysql)/seq-crud-exam/.eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: ['airbnb-base', 'plugin:node/recommended', 'prettier'],
-  parserOptions: {
-    sourceType: 'module',
-  },
-}
